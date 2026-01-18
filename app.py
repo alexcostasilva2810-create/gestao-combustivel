d15ccc0e83041f5d5a6edb3e55f37c26706004f2
zion tecnologia <alexcostasilva2810@gmail.com>
2026-01-18 17:24:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ st.markdown(
         <link rel="manifest" href="/static/manifest.json">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
-        <link rel="apple-touch-icon" href="/static/icon-192.png">
+        <link rel="apple-touch-icon" href="/static/icone ZION.png">
         <title>ZION</title>
     </head>
     """,
